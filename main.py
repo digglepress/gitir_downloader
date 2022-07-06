3b673c3a944a29c024a472d1e4bba2691a499d95
victor bekee <youngtech412@gmail.com>
2022-07-06 16:04:12 +0100

    downloads simplified

--- main.py
+++ main.py
@@ -1,48 +1,31 @@
 import os
+import urllib.request
 from dataclasses import dataclass
-import tkinter as tk
-from tkinter import filedialog, Text
-
-root = tk.Tk()
 
 
-def open_file():
-    file = filedialog.askopenfilename(initialdir='~', title="Open file containing links")
-    print(file)
+@dataclass()
+class GitIrDownloader:
+    base_url: str
+
+    def __post_init__(self):
+        os.chdir('/media/Storage/')
+        data = urllib.request.urlopen(self.base_url).read()
+        self.file_url = data.decode('utf-8').split("\n")[3]
+        file_name = self.file_url.split("/")[4].replace("%20", " ").split("-")[0]
+        try:
+            os.mkdir(os.path.join(os.curdir, file_name))
+        except FileExistsError:
+            pass
+        os.chdir(os.path.join(os.getcwd(), file_name))
+
+    def download(self):
+        os.system(f'wget -nc -c -i {self.base_url}')
 
 
-def perform_download():
-    print('download file')
-
-
-canvas = tk.Canvas(root, height=200, width=400, bg='#222222')
-canvas.pack()
-
-# frame = tk.Frame(root, bg='#222')
-# frame.place(relwidth=0.8, relheight=0.8, relx=0.1, rely=0.1)
-
-open_file = tk.Button(root, text='Select File', fg='#fff', bg='#12488B', command=open_file)
-open_file.pack()
-
-download = tk.Button(root, text='Download', fg='#fff', bg='#12488B', command=perform_download)
-download.pack()
-
-
-class GitIrDownloader(tk.Frame):
-    pass
-
+file = GitIrDownloader(
+    'https://git.ir/media/download-links/udemy-the-ultimate-authentication-course-with-djan'
+    '-258d22a936c54232867c0bcd31e3363c.txt'
+)
 
 if __name__ == "__main__":
-    root.mainloop()
-
-# @dataclass(frozen=True)
-# class GitIrDownloader:
-#     base_url: str
-#
-#     def __post_init__(self):
-#         os.system(f'wget -nc -c -i {self.base_url}')
-
-# GitIrDownloader(
-#     'https://git.ir/media/download-links/linkedin-learning-the-python-3-standard-library'
-#     '-86fa4a7a166e4cc39564a3923111eb3d.txt'
-#     )
+    file.download()
